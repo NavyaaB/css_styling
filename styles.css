6d4b9b21ee2286cc2c7de0b8dd4bc57f6c261c43
NavyaBachati <navyabachati@gmail.com>
2020-01-29 18:51:53 -0500

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,65 @@
+//this a simple file showing inline css styles for a react component
+import React from 'react';
+import './styles.css';
+
+const outerBox = {
+  backgroundColor: 'blue',
+  width: 200,
+  height: 200,
+  marginLeft: "20%",
+  overflow: "hidden"
+}
+const innerBox = {
+  backgroundColor: 'green',
+  width: "50%",
+  height: "50%",
+  position: "relative",
+  left: "25%",
+  top: "25%",
+  
+}
+const innerinnerBox = {
+  backgroundColor: 'yellow',
+  width: "50%",
+  height: "50%",
+  position: "relative",
+  left: "25%",
+  top: "25%",
+  
+}
+const paraStyle = {
+  textAlign: "center",
+  color: "blue",
+  fontFamily:"verdana",
+  fontSize: 20,
+  borderStyle: "solid",
+  borderColor: "red",
+  borderWidth: 1,
+  padding: 20,
+  opacity: 0.5
+}
+
+const StylesApp = () => {
+  return (
+    <div>
+    <div className="outer-box" style={outerBox}>
+      <div className="inner-box" style={innerBox}>
+        <div className="inner-inner-box" style={innerinnerBox}>
+          
+        </div>
+      </div>
+    </div>
+    <div>
+      <p style={paraStyle}>React is a JavaScript library.</p>
+    </div>
+    <div className="outer">
+      <div className="inner">
+        <div className="inner-inner">
+          Flex items
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+};
+export default StylesApp;
